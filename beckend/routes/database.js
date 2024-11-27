e195a170690400d2dd62fedf9fc43aebae9a48c5
Angelo Netti <angelonetti@MacBook-Air-di-Angelo.local>
2024-11-27 19:25:13 +0100

    Commit dei file backend

---
+++ beckend/routes/database.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;  // Flag per evitare connessioni multiple
+
+export default async function handler(req, res) {
+  if (isConnected) {
+    console.log("Already connected to the database");
+    return res.status(200).json({ message: "Already connected" });
+  }
+
+  try {
+    // Connessione al database
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log('Connected to the database');
+    return res.status(200).json({ message: "Connected to database" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Database connection failed" });
+  }
+}
