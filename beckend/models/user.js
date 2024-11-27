c10d5893c4a174791f17b5c465c96d030f55d930
Angelo Netti <angelonetti@MacBook-Air-di-Angelo.local>
2024-11-27 21:15:07 +0100

    Aggiornamento di tutte le cartelle

--- beckend/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const userSchema = mongoose.Schema({
-  username: String,
-  firstName: String,
-  lastName: String
-})
-
-module.exports = mongoose.model("User", userSchema)
