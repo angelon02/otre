b99fa0b280603d949e38476a118a29d2d05b93e7
Angelo <angelonetti990.com>
2024-11-30 14:01:13 +0100

    Aggiunta rout

---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const usersController = require('../controllers/users')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.json({message: 'root for users api'})
+})
+router.post('/add', usersController.addUser)
+router.post('/login', usersController.loginUser)
+
+module.exports = router
